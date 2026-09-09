13db6a86f915e46c726bbe917a590c267758704d
Lars Artmann <git@lars.software>
2026-09-09 02:26:09 +0200

    chore: auto-commit 1 changed file(s) (heuristic)

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
 
       go-standard = {
         pname = "go-humanize-linter";
-        vendorHash = "sha256-OrFJeyX9v1gTjFA+CuooJaK5HlfTptqf4JIpxx7LF/Q=";
+        vendorHash = "sha256-ao8iSe6YuJ1e5vfhVvu8BZyWvpppK+DmyiaKQfon0qg=";
         description = "AST linter that detects hand-rolled reimplementations of go-humanize";
         enableCheck = false;
         subPackages = [ "cmd/go-humanize-linter" ];
